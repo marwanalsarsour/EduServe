a48cb82fbba69bb8206af215cf0a20312bbef494
tameralqadi <221143@ppu.edu.ps>
2026-03-07 16:45:14 +0200

    Update

--- src/public/index.php
+++ src/public/index.php
@@ -1,61 +1,59 @@
 <?php
-// ini_set('display_errors', 1);
-// error_reporting(E_ALL);
+ ini_set('display_errors', 1);
+ error_reporting(E_ALL);
 
-// define('BASE_PATH', dirname(__DIR__));
-// define('VIEW_PATH', BASE_PATH . '/views');
-// define('APP_PATH', BASE_PATH . '/app');
+ define('BASE_PATH', dirname(__DIR__));
+ define('VIEW_PATH', BASE_PATH . '/views');
+ define('APP_PATH', BASE_PATH . '/app');
 
-// require_once APP_PATH . '/Core/Router.php';
+ require_once APP_PATH . '/Core/Router.php';
 
-// $router = new Router();
-
-
-// $router->add('GET', '/', function() {
-//     require_once VIEW_PATH . '/auth/login.view.php';
-// });
+ $router = new Router();
+ $router->add('GET', '/', function() {
+     require_once VIEW_PATH . '/auth/login.view.php';
+ });
 
 
-// $router->add('GET', '/login', function() {
-//     require_once VIEW_PATH . '/auth/login.view.php';
-// });
+ $router->add('GET', '/login', function() {
+     require_once VIEW_PATH . '/auth/login.view.php';
+ });
 
 
-// $router->add('GET', '/student_dashboard', function() {
-//     require_once VIEW_PATH . '/student/student_dashboard.php';
-// });
+ $router->add('GET', '/student_dashboard', function() {
+     require_once VIEW_PATH . '/student/student_dashboard.php';
+ });
 
 
-// $router->add('GET', '/student_opportunities', function() {
-//     require_once VIEW_PATH . '/student/student_opportunities.php';
-// });
+ $router->add('GET', '/student_opportunities', function() {
+     require_once VIEW_PATH . '/student/student_opportunities.php';
+ });
 
-// $router->add('GET', '/student_attendance', function() {
-//     require_once VIEW_PATH . '/student/student_attendance.php';
-// });
+ $router->add('GET', '/student_attendance', function() {
+     require_once VIEW_PATH . '/student/student_attendance.php';
+ });
 
-// $router->add('GET', '/student_reports', function() {
-//     require_once VIEW_PATH . '/student/student_reports.php';
-// });
+ $router->add('GET', '/student_reports', function() {
+     require_once VIEW_PATH . '/student/student_reports.php';
+ });
 
-// $router->add('GET', '/student_apply', function() {
-//     require_once VIEW_PATH . '/student/student_apply.php';
-// });
+ $router->add('GET', '/student_apply', function() {
+     require_once VIEW_PATH . '/student/student_apply.php';
+ });
 
-// $router->add('GET','/student_my-application', function(){
-//     require_once VIEW_PATH . '/student/student_my-application.php';
-// });
+ $router->add('GET','/student_my-application', function(){
+     require_once VIEW_PATH . '/student/student_my-application.php';
+ });
 
-// $router->add('GET','/student_opportunity-details', function(){
-//     require_once VIEW_PATH . '/student/student_opportunity-details.php';
-// });
+ $router->add('GET','/student_opportunity-details', function(){
+     require_once VIEW_PATH . '/student/student_opportunity-details.php';
+ });
 
-// $router->add('GET','/student_report-submit', function(){
-//     require_once VIEW_PATH . '/student/student_report-submit.php';
-// });
-// $router->add('POST', '/login_process', function() {
-//     require_once APP_PATH . '/Controllers/AuthController.php';
-//     $auth = new AuthController();
-//     $auth->login();
-// });
-// $router->run();
+ $router->add('GET','/student_report-submit', function(){
+     require_once VIEW_PATH . '/student/student_report-submit.php';
+ });
+ $router->add('POST', '/login_process', function() {
+     require_once APP_PATH . '/Controllers/AuthController.php';
+     $auth = new AuthController();
+     $auth->login();
+ });
+ $router->run();
